69debf5a4df0ec233666f768eb26faa3489910f6
Ilma Zaqi <redmi.zaqi@gmail.com>
2026-04-17 19:35:10 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,7 @@
 const cacheName = 'v1';
 const assets = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/app.js' // Daftarkan semua file yang ingin disimpan offline
+  './',
+  './index.html',
+  './manifest.json'
 ];
-
-// Install Service Worker
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
-});
-
-// Fetch konten saat offline
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
-});
+// Sisanya tetap sama...
