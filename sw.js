ab2342797fff29bff9930e69f795a406136a003d
Ilma Zaqi <redmi.zaqi@gmail.com>
2026-04-17 20:02:00 +0700

    Implement install and fetch event listeners
    Add service worker event listeners for install and fetch.

--- sw.js
+++ sw.js
@@ -4,4 +4,12 @@ const assets = [
   './index.html',
   './manifest.json'
 ];
-// Sisanya tetap sama...
+
+// Bagian sisanya tetap sama
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
